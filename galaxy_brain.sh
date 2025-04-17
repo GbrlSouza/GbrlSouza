d68f24bc47c03a07e29cebaabc3fd2187bab74d0
Gabriel Souza <gabrielsouza_1909@hotmail.com>
2025-04-17 13:34:53 -0300

    Update galaxy_brain.sh

--- galaxy_brain.sh
+++ galaxy_brain.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-REPO_NAME="teste-galaxy-brain"
+REPO_NAME="badge-galaxy-brain"
 DISCUSSION_TITLE="Como fazer um loop infinito bonito em JavaScript?"
 DISCUSSION_BODY="Tô tentando escrever um loop infinito com estilo. Alguma sugestão?"
 ANSWER_BODY='while(true) console.log("bonito é ser infinito, não infinito ser bonito");'
 
-echo "===> [Secundária] Criando repositório com Discussions habilitado..."
+echo "===> Criando repositório com Discussions habilitado..."
 gh repo create $REPO_NAME --public --enable-discussions --confirm
 
-cd $REPO_NAME
+cd $REPO_NAME || exit
 
-echo "===> [Secundária] Criando discussão..."
+echo "===> Criando discussão..."
+REPO_ID=$(gh api repos/$(gh repo view --json nameWithOwner -q .nameWithOwner) | jq -r .id)
+CATEGORY_ID=$(gh api repos/$(gh repo view --json nameWithOwner -q .nameWithOwner)/discussions/categories | jq -r '.[0].id')
+
 DISCUSSION_URL=$(gh api graphql -f query='mutation($repoId: ID!, $title: String!, $body: String!, $categoryId: ID!) {
   createDiscussion(input: {
     repositoryId: $repoId,
@@ -22,11 +25,8 @@ DISCUSSION_URL=$(gh api graphql -f query='mutation($repoId: ID!, $title: String!
       url
     }
   }
-}' -F repoId=$(gh api repos/$(gh repo view --json nameWithOwner -q .nameWithOwner) | jq -r .id) \
-   -F title="$DISCUSSION_TITLE" -F body="$DISCUSSION_BODY" \
-   -F categoryId=$(gh api repos/$(gh repo view --json nameWithOwner -q .nameWithOwner)/discussions/categories | jq -r '.[0].id') \
-   | jq -r '.data.createDiscussion.discussion.url')
+}' -F repoId=$REPO_ID -F title="$DISCUSSION_TITLE" -F body="$DISCUSSION_BODY" -F categoryId=$CATEGORY_ID \
+| jq -r '.data.createDiscussion.discussion.url')
 
 echo "===> Acesse com a conta principal e responda aqui:"
 echo "$DISCUSSION_URL"
-echo "===> Após responder, com a conta secundária, marque a resposta como 'melhor resposta'."
